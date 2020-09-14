6b5658b0a423d4ac04c98a468af7dac2f70e75ae
A ruiz <luis.alonso.16@hotmail.com>
2020-09-14 00:00:12 -0600

    Add multi error to user entity

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -3,6 +3,7 @@ package entity
 import (
 	"github.com/alexandria-oss/common-go/exception"
 	"github.com/alexandria-oss/podcast-api/internal/domain/value"
+	"github.com/hashicorp/go-multierror"
 )
 
 // User identity
@@ -23,19 +24,21 @@ func (c User) IsValid() error {
 	// * Foreign data
 	// - Required: ID, Display Name
 	if c.DisplayName.Get() == "" {
-		return exception.NewRequiredField("user_name")
+		return exception.NewRequiredField("user_display_name")
 	} else if c.ID.Get() == "" {
 		return exception.NewRequiredField("user_id")
 	}
 
 	// Value Object
+	var valueErr error
 	if err := c.DisplayName.IsValid(); err != nil {
-		return err
-	} else if c.Image != nil {
-		if errImg := c.Image.IsValid(); errImg != nil {
-			return errImg
+		valueErr = multierror.Append(valueErr, err)
+	}
+	if c.Image != nil {
+		if err := c.Image.IsValid(); err != nil {
+			valueErr = multierror.Append(valueErr, err)
 		}
 	}
 
-	return nil
+	return valueErr
 }
