55b2a898e9bca55b052698bbd341935867e83fc4
A ruiz <luis.alonso.16@hotmail.com>
2020-09-13 18:30:34 -0600

    Export podcast entity attributes

--- internal/domain/entity/podcast.go
+++ internal/domain/entity/podcast.go
@@ -16,10 +16,16 @@ package entity
 
 // Podcast Podcasts entity
 type Podcast struct {
-	RssID       string `json:"rss_id"`
-	DurationSec int64  `json:"duration_sec"`
-	Duration    string `json:"duration"`
-	Episode     int32  `json:"episode"`
-	EpisodeType string `json:"episode_type"`
-	Transcript  string `json:"transcript"`
+	// RssID RSS feed item GUID
+	RssID string
+	// DurationSec total duration in seconds
+	DurationSec int64
+	// Duration total duration in hh:mm:ss format
+	Duration string
+	// Episode episode number
+	Episode int32
+	// EpisodeType kind of episode
+	EpisodeType string
+	// Transcript content's transcript url
+	Transcript string
 }
